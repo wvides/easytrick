72629fb8de55dab0ac8d4f7196d73023fd14fac8
omarvides <omarvides@gmail.com>
2013-06-04 22:17:44 -0800

    Repo creation

---
+++ src/easytrick/logic/EasyTrick.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easytrick.logic;
+
+import easytrick.view.EasyJform;
+import easytrick.view.binaryLogic;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Omar
+ */
+public class EasyTrick {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EasyJform e = new EasyJform();
+        e.setVisible(true);
+    }
+}
